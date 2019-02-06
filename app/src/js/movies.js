8bfc3d4ca5930a42e627e9291517c73efb4f99e6
Sebastian <sebastian@hamse.fi>
2019-02-06 19:27:30 +0200

    Select functionality works for theaters.

--- app/src/js/movies.js
+++ app/src/js/movies.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  movieData();
-})
+  movieTheatres();
+});
 
 const timeConvert = (time) => {
   const givenMinutes = time;
@@ -11,33 +11,60 @@ const timeConvert = (time) => {
   return `${rhours}h ${rminutes}min`;
 }
 
-const movieData = () => {  
+const movieTheatres = () => {  
+  let movieTheatres = [];
+  
   $.ajax({
     url: "http://www.finnkino.fi/xml/Schedule/",
     dataType: "xml",
     success: function(data) {
       $(data).find("Shows Show").each(function() {
-        const EventID = $(this).find("EventID").text();
-        const Title = $(this).find("Title").text();
-        const OriginalTitle = $(this).find("OriginalTitle").text();
-        const PosterImage = $(this).find("Images EventMediumImagePortrait").text();
-        const RatingImageUrl = $(this).find("RatingImageUrl").text();
-        let ShowStart = $(this).find("dttmShowStartUTC").text();
-        const ShowStartTime = ShowStart.split("T")[1].slice(0, -4);
-        const LengthInMinutes = $(this).find("LengthInMinutes").text();
+        const TheatreID = $(this).find("TheatreID").text();
+        const Theatre = $(this).find("Theatre").text();
 
-        const movieListItem = $("<li></li>", {class: "movie-listing__item" });
-        const movieElement = $("<div></div>", {id: EventID, class: "movie" }).appendTo(movieListItem);
-        const movieImagesElement = $("<div></div>", {class: "movie__images" }).appendTo(movieElement);
-        const posterImageElement = $("<img></img>", {class: "movie__images-poster", alt: "poster image", src: PosterImage }).appendTo(movieImagesElement);
-        const ratingImageElement = $("<img></img>", {class: "movie__images-restriction", alt: "age restriction", src: RatingImageUrl }).appendTo(movieImagesElement);
-        const movieInformationElement = $("<div></div>", {class: "movie__information" }).appendTo(movieElement);
-        const titleElement = $("<h2></h2>", {class: "movie__information-name", text: Title }).appendTo(movieInformationElement);
-        const titleOriginalElement = $("<h3></h3>", {class: "movie__information-name-original", text: OriginalTitle }).appendTo(movieInformationElement);
-        const showStartElement = $("<p></p>", {class: "movie__information-start-time", text: `START TIME: ${ShowStartTime}` }).appendTo(movieInformationElement);
-        const movieLengthElement = $("<p></p>", {class: "movie__information-length", text: `DURATION: ${timeConvert(LengthInMinutes)}` }).appendTo(movieInformationElement);
+        movieTheatres.push(Theatre);
+        const selectElement = $("#theatres");
+        const optionElement = $("<option></option>", {id: TheatreID, class: "theatre-listing__item", text: Theatre }).appendTo(selectElement);
+      });
+    }
+  });
 
-        movieListItem.appendTo(".js-movie-listing");
+  $("#theatres").on('change', function(){
+    const selectedTheatre = $(this).val();    
+    $('.movie-listing__item').remove();
+    movieData(selectedTheatre);
+  });
+}
+
+const movieData = (theatre) => {  
+  $.ajax({
+    url: "http://www.finnkino.fi/xml/Schedule/",
+    dataType: "xml",
+    success: function(data) {
+      $(data).find("Shows Show").each(function() {
+        if (theatre === $(this).find("Theatre").text()) {
+          const EventID = $(this).find("EventID").text();
+          const Title = $(this).find("Title").text();
+          const OriginalTitle = $(this).find("OriginalTitle").text();
+          const PosterImage = $(this).find("Images EventMediumImagePortrait").text();
+          const RatingImageUrl = $(this).find("RatingImageUrl").text();
+          let ShowStart = $(this).find("dttmShowStartUTC").text();
+          const ShowStartTime = ShowStart.split("T")[1].slice(0, -4);
+          const LengthInMinutes = $(this).find("LengthInMinutes").text();
+
+          const movieListItem = $("<li></li>", {class: "movie-listing__item" });
+          const movieElement = $("<div></div>", {id: EventID, class: "movie" }).appendTo(movieListItem);
+          const movieImagesElement = $("<div></div>", {class: "movie__images" }).appendTo(movieElement);
+          const posterImageElement = $("<img></img>", {class: "movie__images-poster", alt: "poster image", src: PosterImage }).appendTo(movieImagesElement);
+          const ratingImageElement = $("<img></img>", {class: "movie__images-restriction", alt: "age restriction", src: RatingImageUrl }).appendTo(movieImagesElement);
+          const movieInformationElement = $("<div></div>", {class: "movie__information" }).appendTo(movieElement);
+          const titleElement = $("<h2></h2>", {class: "movie__information-name", text: Title }).appendTo(movieInformationElement);
+          const titleOriginalElement = $("<h3></h3>", {class: "movie__information-name-original", text: OriginalTitle }).appendTo(movieInformationElement);
+          const showStartElement = $("<p></p>", {class: "movie__information-start-time", text: `START TIME: ${ShowStartTime}` }).appendTo(movieInformationElement);
+          const movieLengthElement = $("<p></p>", {class: "movie__information-length", text: `DURATION: ${timeConvert(LengthInMinutes)}` }).appendTo(movieInformationElement);
+
+          movieListItem.appendTo(".js-movie-listing");
+        }
       });
     }
   });
